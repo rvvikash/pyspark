47b98a3a0b18d6ff0780d1db1413c12e43fce2f8
rvvikash <38788306+rvvikash@users.noreply.github.com>
2024-09-29 11:43:57 +0530

    Update impolinks.py

--- impolinks.py
+++ impolinks.py
@@ -8,4 +8,7 @@ https://spark.apache.org/docs/3.1.3/api/python/reference/api/pyspark.sql.DataFra
 
 https://github.com/spark-examples/pyspark-examples/tree/master
 
+https://k21academy.com/data-engineering/pyspark-interview-questions/
+
+https://www.projectpro.io/article/pyspark-interview-questions-and-answers/520
 
